5948afb64a1cc574fa8e94dadcb955f2cba04258
Ansar <ansaraws2020@gmail.com>
2022-09-12 13:04:47 +0400

    initial commit

---
+++ main.tf
@@ -0,0 +1,80 @@
+#------------------------------------------------#-----------------------------------------------------------------
+
+
+########################    Terraform configuration       ###############################
+
+#--------------------------------------------------#----------------------------------------------------------------
+
+
+
+terraform {
+  required_version = ">= 1.1.7"
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "~> 4.2"
+    }
+  }
+  backend "remote" {
+    organization = "soban252"
+
+    workspaces {
+      name = "eks-testing"
+    }
+
+}
+}
+provider "aws" {
+  region = var.region
+
+
+}
+
+
+#------------------------------------------------#-----------------------------------------------------------------
+
+
+########################    Terraform modules      ###############################
+
+#--------------------------------------------------#----------------------------------------------------------------
+
+
+
+module "vpc" {
+  source   = "./modules/vpc"
+  vpc_cidr = var.vpc_cidr
+  vpc_name = var.vpc_name
+  # public_subnets_cidr = var.public_subnets_cidr
+  # private_subnets_cidr = var.private_subnets_cidr
+  # azs = var.azs
+
+
+
+}
+
+
+
+module "rds" {
+  source     = "./modules/rds"
+  vpc_id     = module.vpc.vpc_id
+  cidr_block = module.vpc.cidr_block
+  subnet_ids = [module.vpc.public-us-east-1a, module.vpc.public-us-east-1b]
+  depends_on = [
+    module.vpc
+  ]
+
+
+
+}
+
+module "eks" {
+  source         = "./modules/eks"
+  vpc_id         = module.vpc.vpc_id
+  eks_subnet_ids = [module.vpc.public-us-east-1a, module.vpc.public-us-east-1b, module.vpc.private-us-east-1a, module.vpc.private-us-east-1b]
+  depends_on = [
+    module.vpc
+  ]
+  eks_node_subnets_ids = [module.vpc.private-us-east-1a, module.vpc.private-us-east-1b]
+}
+
+
---
+++ modules/vpc/main.tf
@@ -0,0 +1,149 @@
+#------------------------------------------------#-----------------------------------------------------------------
+
+
+########################    VPC        ###############################
+
+#--------------------------------------------------#----------------------------------------------------------------
+
+#Creating VPC
+resource "aws_vpc" "main" {
+  cidr_block       = var.vpc_cidr
+  instance_tenancy = "default"
+  enable_dns_hostnames=true
+  enable_dns_support = true
+
+  tags = {
+    Name = var.vpc_name
+  }
+}
+
+resource "aws_internet_gateway" "igw" {
+  vpc_id = aws_vpc.main.id
+
+  tags = {
+    Name = "igw"
+  }
+}
+resource "aws_eip" "nat" {
+  vpc = true
+
+  tags = {
+    Name = "nat"
+  }
+}
+
+resource "aws_nat_gateway" "nat" {
+  allocation_id = aws_eip.nat.id
+  subnet_id     = aws_subnet.public-us-east-1a.id
+
+  tags = {
+    Name = "nat"
+  }
+
+  depends_on = [aws_internet_gateway.igw]
+}
+
+
+
+resource "aws_subnet" "private-us-east-1a" {
+  vpc_id            = aws_vpc.main.id
+  cidr_block        = "10.0.1.0/24"
+  availability_zone = "us-east-1a"
+
+  tags = {
+    "Name"                            = "private-us-east-1a"
+    "kubernetes.io/role/internal-elb" = "1"
+    "kubernetes.io/cluster/demo"      = "owned"
+  }
+}
+
+resource "aws_subnet" "private-us-east-1b" {
+  vpc_id            = aws_vpc.main.id
+  cidr_block        = "10.0.2.0/24"
+  availability_zone = "us-east-1b"
+  tags = {
+    "Name"                            = "private-us-east-1b"
+    "kubernetes.io/role/internal-elb" = "1"
+    "kubernetes.io/cluster/demo"      = "owned"
+  }
+}
+
+resource "aws_subnet" "public-us-east-1a" {
+  vpc_id                  = aws_vpc.main.id
+  cidr_block              = "10.0.3.0/24"
+  availability_zone       = "us-east-1a"
+  map_public_ip_on_launch = true
+
+  tags = {
+    "Name"                       = "public-us-east-1a"
+    "kubernetes.io/role/elb"     = "1"
+    "kubernetes.io/cluster/demo" = "owned"
+  }
+}
+
+resource "aws_subnet" "public-us-east-1b" {
+  vpc_id                  = aws_vpc.main.id
+  cidr_block              = "10.0.4.0/24"
+  availability_zone       = "us-east-1b"
+  map_public_ip_on_launch = true
+
+  tags = {
+    "Name"                       = "public-us-east-11b"
+    "kubernetes.io/role/elb"     = "1"
+    "kubernetes.io/cluster/demo" = "owned"
+  }
+}
+resource "aws_route_table" "private" {
+  vpc_id = aws_vpc.main.id
+
+  route {
+    cidr_block = "0.0.0.0/0"
+    nat_gateway_id  = aws_nat_gateway.nat.id
+  }
+
+  
+
+  tags = {
+    Name = "private"
+  }
+}
+
+
+resource "aws_route_table" "public" {
+  
+
+  vpc_id = aws_vpc.main.id
+    route {
+
+        cidr_block = "0.0.0.0/0"
+        gateway_id = aws_internet_gateway.igw.id
+    }
+  
+
+  tags = {
+    Name = "public"
+  }
+}
+
+resource "aws_route_table_association" "private-us-east-1a" {
+  subnet_id      = aws_subnet.private-us-east-1a.id
+  route_table_id = aws_route_table.private.id
+}
+
+resource "aws_route_table_association" "private-us-east-1b" {
+  subnet_id      = aws_subnet.private-us-east-1b.id
+  route_table_id = aws_route_table.private.id
+}
+
+resource "aws_route_table_association" "public-us-east-1a" {
+  subnet_id      = aws_subnet.public-us-east-1a.id
+  route_table_id = aws_route_table.public.id
+}
+
+resource "aws_route_table_association" "public-us-east-1b" {
+  subnet_id      = aws_subnet.public-us-east-1b.id
+  route_table_id = aws_route_table.public.id
+}
+
+
+
