a90d43b475d61e099570ac1f2098d5202f127763
Ansar <ansaraws2020@gmail.com>
2022-09-16 10:55:45 +0400

    changes was made

--- modules/eks/main.tf
+++ modules/eks/main.tf
@@ -159,7 +159,7 @@ data "aws_iam_policy_document" "aws_load_balancer_controller_assume_role_policy"
 
 resource "aws_iam_role" "aws_load_balancer_controller" {
   assume_role_policy = data.aws_iam_policy_document.aws_load_balancer_controller_assume_role_policy.json
-  name               = "aws-load-balancer-controller"
+  name               = "aws-load-balancer-controller-${var.region}" #change was made to test Terraform implimentation
 }
 
 resource "aws_iam_policy" "aws_load_balancer_controller" {
